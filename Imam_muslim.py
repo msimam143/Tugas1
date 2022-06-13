a5f523474028a931d4ef445c1e5800db3e9587fb
msimam143 <107403860+msimam143@users.noreply.github.com>
2022-06-13 17:39:14 +0700

    Add files via upload

---
+++ Imam_muslim.py
@@ -0,0 +1,51 @@
+#membuat data mahasiswa
+
+print("==============================")
+print("=====Biodata mahasiswa=====")
+print("==============================")
+
+Nama = str(input("masukan Nama:"))
+Alamat = str(input("masukan Alamat:"))
+Jurusan = str(input("masukan Jurusan:"))
+Nim = str(input("masukan Nim:"))
+
+print("============")
+print("Nama:",Nama)
+print("Alamat:",Alamat)
+print("Jurusan:",Jurusan)
+print("Nim:", Nim)
+print("===data berhasil di tambah===") 
+
+nilai = int(input('masukan nilai anda:'))
+
+if nilai >= 90:
+    print('predikat A')
+elif nilai >= 80:
+    print('predikat B')
+elif nilai >= 60:
+    print('predikat C')
+elif nilai >= 40:
+    print('predikat D')
+else:
+    print('predikat E')
+
+print('nilai anda adalah:', nilai, '\n')
+
+if nilai >=70:
+    print('selamat, anda lulus!')
+else:
+    print('maaf, anda tidak lulus.')
+
+class mahasiswa:
+    def __init__(self, semester):
+        self.semester = semester
+        
+
+class mahasiswa:
+    pass
+    ('semester')
+
+print('semester 2')
+
+
+
